94e1b89e3fbd72790612e5c93715b8b913fb31ae
zman <manzhiyuan@weidian.com>
2016-07-13 11:32:12 +0800

    修改refactor：根据group和projectName创建工作目录

--- refactor.sh
+++ refactor.sh
@@ -7,6 +7,13 @@ projectName=$2
 
 #替换group
 sed -i "" "s/com.zmannotes.spring/$groupName/" pom.xml
+#移动文件到新的目录，目录为group和projectName拼接而成路径
+packageName=$groupName/`echo ${projectName//-\/}`
+dir=`echo ${packageName//\./\/}`
+mkdir -p src/main/java/$dir
+mv src/main/java/com/zmannotes/spring/* src/main/java/$dir
+#删除旧目录
+rm -rf src/main/java/com/zmannotes
 #替换projectName
 sed -i "" "s/spring-basic-application/$projectName/g" pom.xml
 mv ../spring-basic-application ../$projectName
